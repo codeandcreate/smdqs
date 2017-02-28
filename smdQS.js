e6379c4b7b5d5c43f63c0f9aeebc8c2aaac17f07
Matthias Weiß <m.weiss@schwaebische.de>
2017-02-28 11:22:02 +0100

    Add files via upload
    New structure and two new functions: requireJS and ready.

--- smdQS.js
+++ smdQS.js
@@ -1,89 +1,93 @@
 /**
  *
- * smdQS - A minimalistic javascript wrapper for the jquery babied
+ * smdQS - A minimalistic javascript object for the jquery babied
  *
- * @version 1.0
+ * @version 2.0-testing
  * @author Matthias Weiß <m.weiss@smdigital.de>
  *
  * @changes		20150000	MW	:	- Initialversion
+ * @changes		20170300	MW	:	- New structure and tools (docReady and loadJS)
  *
  */
-
-/**
- * Initializer for prototype funktions hasClass, addClass, removeClass, toggleClass
- */
-function smdQSinitHtmLElements()
-{
-	var elementPrototype = typeof HTMLElement !== "undefined" ? HTMLElement.prototype : (typeof Element !== "undefined" ? Element.prototype : null);
-
-	if (elementPrototype != null) {
-		elementPrototype.removeClass = function (remove)
-		{
-			var newClassName = "";
-			var i;
-			var classes      = this.className.split(" ");
-			for (i = 0; i < classes.length; i++) {
-				if (classes[i] !== remove) {
-					newClassName += classes[i] + " ";
+(function(funcName, baseObj) {
+	funcName = funcName || "smdQS";
+	baseObj = baseObj || window;
+	
+	var __init = false;
+	var __readyList = [];
+	var __readyFired = false;
+	var __readyEventHandlersInstalled = false;
+	
+	/**
+	 * Initializer for prototype funktions hasClass, addClass, removeClass, toggleClass
+	 */
+	function _smdQSinitHtmLElements()
+	{
+		var elementPrototype = typeof HTMLElement !== "undefined" ? HTMLElement.prototype : (typeof Element !== "undefined" ? Element.prototype : null);
+	
+		if (elementPrototype != null) {
+			elementPrototype.removeClass = function (remove)
+			{
+				var newClassName = "";
+				var i;
+				var classes      = this.className.split(" ");
+				for (i = 0; i < classes.length; i++) {
+					if (classes[i] !== remove) {
+						newClassName += classes[i] + " ";
+					}
 				}
-			}
-			this.className = newClassName.trim();
-		};
-		elementPrototype.addClass    = function (add)
-		{
-			if (this.className.indexOf(add) == -1) {
-				this.className = this.className + " " + add;
-			}
-		};
-		elementPrototype.toggleClass = function (toggle)
-		{
-			if (this.className.indexOf(toggle) == -1) {
-				this.addClass(toggle);
-			} else {
-				this.removeClass(toggle);
-			}
-		};
-		elementPrototype.hasClass    = function (classToCheck)
-		{
-			return (this.className.indexOf(classToCheck) != -1);
-		};
-	}
-}
-
-/**
- * smdQS
- *
- * Subfunctions and Unterfunktionen und properties:
- *
- * ajax()		-	jQuery.ajax() Equivalent (doesn't need a valid DOM node)
- * forEach()		-	go through a list of nodes
- * isDomObject		-	true / false: Shows, if one or more nodes are found
- * isList		- 	true / false: If more than one nodes are found, this is "true" and you have to use "forEach()"
- *
- * @param selector		CSS/jQuery like Selektor (uses querySelectorAll)
- * @param baseObj		base node. if none is given, "document" is used
- * @returns {{}}
- */
-var smdQS = function(selector, baseObj)
-{
+				this.className = newClassName.trim();
+			};
+			elementPrototype.addClass    = function (add)
+			{
+				if (this.className.indexOf(add) == -1) {
+					this.className = this.className + " " + add;
+				}
+			};
+			elementPrototype.toggleClass = function (toggle)
+			{
+				if (this.className.indexOf(toggle) == -1) {
+					this.addClass(toggle);
+				} else {
+					this.removeClass(toggle);
+				}
+			};
+			elementPrototype.hasClass    = function (classToCheck)
+			{
+				return (this.className.indexOf(classToCheck) != -1);
+			};
+		}
+	};
+	
 	/**
 	 * jQuery.ajax() Equivalent
 	 *
-	 * @param url			URL which will be called (without GET params!)
-	 * @param callback		Callback function, that gets the responseText if status 200 or 201 is returned
-	 * @param data			data as array or string (...=...&...=...)
-	 * @param method		GET (default), POST, PUT, DELETE
+	 * @param url           URL which will be called (without GET params!)
+	 * @param callback      Callback function, that gets the responseText if status 200 or 201 is returned
+	 * @param data          data as array or string (...=...&...=...)
+	 * @param method        GET (default), POST, PUT, DELETE
 	 */
-	function ajax(url, callback, data, method)
-	{
+	function _ajax(url, callback, data, method)
+	{	
+		if (typeof url === "object") {
+			var ajaxCallObject = url;
+			url                = ajaxCallObject.url || undefined;
+			callback           = ajaxCallObject.callback || undefined;
+			data               = ajaxCallObject.data || "";
+			method             = ajaxCallObject.method || "GET";
+		}
+		
+		if (typeof url == "undefined" || typeof callback == "undefined") {
+			return false;
+		}
+	
 		var xmlHttp = null;
-
+	
 		if (window.XMLHttpRequest) {
 			xmlHttp = new XMLHttpRequest();
-		} else
-			if (window.ActiveXObject) {
-				xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
-			}
+		} else if (window.ActiveXObject) {
+			xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+		}
 
 		if (xmlHttp != null) {
 			function readyStateHandler(e)
@@ -105,10 +109,10 @@ var smdQS = function(selector, baseObj)
 			}
 
 			if (typeof data == "object") {
-				var tmp = data;
-				data    = "";
-				for (var key in tmp) {
-					data = data + "&" + key + "=" + encodeURIComponent(tmp[key]);
+				var dataObject = data;
+				data           = "";
+				for (var key in dataObject) {
+					data = data + "&" + key + "=" + encodeURIComponent(dataObject[key]);
 				}
 			}
 
@@ -124,16 +128,94 @@ var smdQS = function(selector, baseObj)
 				xmlHttp.send();
 			}
 		}
-	}
-
-	var element         = {};
-	element.isDomObject = false;
-	element.isList      = false;
-
-	if (selector != "" && selector != null && typeof selector == "string") {
-		if (!baseObj) {
+	};
+	
+	/**
+	 * Based on loadJS; 
+	 * inserts a script-tag and executes an optional callback onload
+	 *
+	 * @param scriptSource	URL of a js-script
+	 * @param callback		optional callback function
+	 */
+	function _requireJS(scriptSource, callback)
+	{	
+		if (typeof scriptSource == "undefined") {
+			return false;
+		}
+	
+		"use strict";
+		var ref = w.document.getElementsByTagName( "script" )[ 0 ];
+		var script = w.document.createElement( "script" );
+		script.src = scriptSource;
+		script.async = true;
+		ref.parentNode.insertBefore( script, ref );
+		if (callback && typeof(callback) === "function") {
+			script.onload = callback;
+		}
+		return script;
+	};
+	
+	/**
+	 * a $( document ).ready({});-like 
+	 * Forked from https://github.com/jfriend00/docReady
+	 *
+	 * @param callback	that will be fired if document is ready
+	 * @param context	optional context
+	 */
+	function _docReady(callback, context)
+	{
+		if (__readyFired) {
+			setTimeout(function() {callback(context);}, 1);
+			return;
+		} else {
+			__readyList.push({fn: callback, ctx: context});
+		}
+		if (document.readyState === "complete") {
+			setTimeout(_documentIsReady, 1);
+		} else if (!__readyEventHandlersInstalled) {
+			if (document.addEventListener) {
+				document.addEventListener("DOMContentLoaded", _documentIsReady, false);
+				window.addEventListener("load", _documentIsReady, false);
+			} else {
+				document.attachEvent("onreadystatechange", readyStateChange);
+				window.attachEvent("onload", _documentIsReady);
+			}
+			__readyEventHandlersInstalled = true;
+		}
+	};
+	
+	/**
+	 * Belongs to _docReady
+	 */
+	function _documentIsReady() {
+		if (!__readyFired) {
+			__readyFired = true;
+			for (var i = 0; i < __readyList.length; i++) {
+				__readyList[i].fn.call(window, __readyList[i].ctx);
+			}
+			__readyList = [];
+		}
+	};
+	
+	/**
+	 * Belongs to _docReady
+	 */
+	function _readyStateChange()
+	{
+		if ( document.readyState === "complete" ) {
+			_documentIsReady();
+		}
+	};
+	
+	/**
+	 * Main function that returns one ore more DOM Objects
+	 */
+	function _smdQSMain(selector, baseObj, element) 
+	{	
+		if (!baseObj || typeof baseObj == "undefined" || typeof baseObj.querySelectorAll == "undefined") {
 			baseObj = document;
 		}
+
 		var domObjects = baseObj.querySelectorAll(selector);
 		if (typeof domObjects === "object" && domObjects != null) {
 			if (domObjects.length == 1) {
@@ -163,9 +245,44 @@ var smdQS = function(selector, baseObj)
 				}
 			}
 		}
-	}
-	element.ajax = ajax;
-	return element;
-};
+		return element;
+	};
+	
+	/**
+	 * smdQS
+	 *
+	 * Subfunctions and Unterfunktionen und properties:
+	 *
+	 * ajax()		-	jQuery.ajax() Equivalent (doesn't need a valid DOM node)
+	 * requireJS()	-	Inserts a script-Tag and runs an callback onload (doesn't need a valid DOM node)
+	 * docReady()	-	
+	 * forEach()	-	go through a list of nodes
+	 * isDomObject	-	true / false: Shows, if one or more nodes are found
+	 * isList		- 	true / false: If more than one nodes are found, this is "true" and you have to use "forEach()"
+	 *
+	 * @param selector		CSS/jQuery like Selektor (uses querySelectorAll)
+	 * @param baseObj		base node. if none is given, "document" is used
+	 * @returns {{}}
+	 */
+	baseObj[funcName] = function(selector, baseObj) 
+	{
+		var element 		= {};
+		element.isDomObject = false;
+		element.isList      = false;
+		
+		if (!__init) {
+			_smdQSinitHtmLElements();
+			__init = true;
+		}
+		
+		if (selector != "" && selector != null && typeof selector == "string") {
+			element = _smdQSMain(selector, baseObj, element);
+		}
+		
+		element.ajax 		= _ajax;
+		element.requireJS	= _requireJS;
+		element.ready		= _docReady;
 
-smdQSinitHtmLElements();
+		return element;
+	};
+})("smdQS", window);
